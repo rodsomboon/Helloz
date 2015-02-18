b812be50eefcd27f3b4ac10c14a18a94b1f54f53
Thanawat Rodsomboon <rodsomboon@gmail.com>
2015-02-18 09:32:25 +0700

    Partial implementation of display number

--- src/com/test/SalesData.java
+++ src/com/test/SalesData.java
@@ -2,8 +2,12 @@ package com.test;
 
 public class SalesData {
 	
+	int data[] = {0, 4, 42};
+	
 	public void display() {
-		
+		for (int i = 0; i < data.length; i++) {
+			System.out.println("");
+		}
 	}
 
 }
